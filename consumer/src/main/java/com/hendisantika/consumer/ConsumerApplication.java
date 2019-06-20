55abead75d804abf60c7b1154429bd9d27dee502
hendisantika <hendisantika@yahoo.co.id>
2019-06-21 06:56:26 +0700

    #12 - Modify Main App in consumer module

--- consumer/src/main/java/com/hendisantika/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/hendisantika/consumer/ConsumerApplication.java
@@ -1,13 +1,23 @@
 package com.hendisantika.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.JmsListener;
 
 @SpringBootApplication
 public class ConsumerApplication {
+    private static final Logger log = LoggerFactory.getLogger(ConsumerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @JmsListener(destination = "${inbound.endpoint}")
+    public void consumedMessageHandler(Person person) {
+        log.info("Id :" + person.getId() + " Name: " + person.getName());
+
     }
 
 }
