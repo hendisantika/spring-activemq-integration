e172de4752ef81bc822ad0098507b17ccf3a95fe
hendisantika <hendisantika@yahoo.co.id>
2019-06-21 06:46:54 +0700

    #8 - Modify Main App in publisher module

--- publisher/src/main/java/com/hendisantika/publisher/PublisherApplication.java
+++ publisher/src/main/java/com/hendisantika/publisher/PublisherApplication.java
@@ -1,13 +1,42 @@
 package com.hendisantika.publisher;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.jms.core.JmsTemplate;
+
+import java.util.UUID;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class PublisherApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PublisherApplication.class, args);
+    }
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Value("${outbound.endpoint}")
+    private String destination;
+
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.springframework.boot.CommandLineRunner#run(java.lang.String[])
+     */
+    @Override
+    public void run(String... arg0) throws Exception {
+        Person person = new Person();
+        person.setId(UUID.randomUUID().toString());
+        person.setName("John Doe");
+
+        jmsTemplate.convertAndSend(destination, person);
+
     }
 
 }
