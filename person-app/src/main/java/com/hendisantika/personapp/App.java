6594b5944d43211afbe3a38c5f5575a9f9167677
hendisantika <hendisantika@yahoo.co.id>
2019-06-21 06:38:43 +0700

    #2 - Add person-app as new module

---
+++ person-app/src/main/java/com/hendisantika/personapp/App.java
@@ -0,0 +1,16 @@
+package com.hendisantika.personapp;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-21
+ * Time: 06:38
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
